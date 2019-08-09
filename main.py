1d6bf3fd325cc611075054de8380dcbfc8b6f276
thesmail <smail.ytn@gmail.com>
2019-08-09 12:36:07 +0300

    Added security auth data Fix code

--- main.py
+++ main.py
@@ -1,38 +1,43 @@
 import requests
+import auth_data
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
+session = requests.Session()
 
 def auth():
-    session = requests.Session()
     url = 'http://taman.trans.efko.ru/login.php'
-    data = {'xLogin':'test', 'xPassword':'test'}
-
-    session.post(url, data=data)
+    session.post(url, data=auth_data.get_pass())
 
 def find():
-    url = "http://y91805lt.beget.tech/"
-    html = requests.get(url, verify=False, headers={'User-Agent': UserAgent(verify_ssl=False).chrome})
+    #url = "http://y91805lt.beget.tech"
+    url = "http://taman.trans.efko.ru/trade/2"
+    urls_bett = []
+    url_logout = "http://taman.trans.efko.ru/logout.php"
+    city = "Анапа"
+
+    html = session.get(url, verify=False, headers={'User-Agent': UserAgent(verify_ssl=False).chrome})
     soup = BeautifulSoup(html.content, 'html.parser')
 
-    trs = soup.find_all('tr')
-    urls_bett = []
+    if soup.find('table') != None:
+        trs = soup.find_all('tr')
+        for tr in trs:
+            if city in tr.text:
+                btns = tr.find_all('button')
+                for btn in btns:
+                    if "10" in btn.text:
+                        urls_bett.append(btn.attrs['href'])
 
-    for tr in trs:
-        if "Анапа" in tr.text:
-            btns = tr.find_all('button')
-            for btn in btns:
-                if "10" in btn.text:
-                    urls_bett.append(btn.attrs['href'])
-
-
-    url_b = "http://taman.trans.efko.ru/trade/2" + urls_bett[1]
-    #requests.get()
-
-
-    print(url_b)
+        url_b = url + urls_bett[0]
+        session.get(url_b, verify=False, headers={'User-Agent': UserAgent(verify_ssl=False).chrome})
+        session.get(url_logout)
+        print("Ставка сделана")
+    else:
+        print("Нет торгов")
+        session.get(url_logout)
 
 def main():
+    auth()
     find()
 
 if __name__ == '__main__':
