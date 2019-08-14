1704b19ab839bc6f72ce3e288b13fa3f67c7de53
thesmail <smail.ytn@gmail.com>
2019-08-15 00:04:14 +0300

    Refactoring

--- main.py
+++ main.py
@@ -1,44 +1,63 @@
 import requests
 import auth_data
+import re
 from bs4 import BeautifulSoup
 from fake_useragent import UserAgent
 
 session = requests.Session()
+urlLogout = "http://taman.trans.efko.ru/logout.php"
+betTask = []
 
 def auth():
     url = 'http://taman.trans.efko.ru/login.php'
     session.post(url, data=auth_data.get_pass())
 
-def find():
-    #url = "http://y91805lt.beget.tech"
+def parser():
+    url = "http://y91805lt.beget.tech"
     url = "http://taman.trans.efko.ru/trade/2"
-    urls_bett = []
-    url_logout = "http://taman.trans.efko.ru/logout.php"
-    city = "Анапа"
 
     html = session.get(url, verify=False, headers={'User-Agent': UserAgent(verify_ssl=False).chrome})
-    soup = BeautifulSoup(html.content, 'html.parser')
+    soup = BeautifulSoup(html.content, 'lxml')
 
     if soup.find('table') != None:
-        trs = soup.find_all('tr')
-        for tr in trs:
-            if city in tr.text:
-                btns = tr.find_all('button')
-                for btn in btns:
-                    if "10" in btn.text:
-                        urls_bett.append(btn.attrs['href'])
+        for rows in soup.find_all('tr')[1:]:
+            cols = rows.find_all('td')
+            btnBet = rows.find('button', class_='newbet')
 
-        url_b = url + urls_bett[0]
-        session.get(url_b, verify=False, headers={'User-Agent': UserAgent(verify_ssl=False).chrome})
-        session.get(url_logout)
-        print("Ставка сделана")
+            if '10' in btnBet.text:
+                btnBet = url + btnBet.attrs['href']
+            else: btnBet = None
+
+            betTask.append({
+                'num': cols[0].strong.text,
+                'date': cols[9].strong.text,
+                'cityOut': cols[20].strong.text,
+                'cityIn': cols[24].strong.text,
+                'urlBet10': btnBet
+            })
+
+        print("Парсинг окончен")
     else:
         print("Нет торгов")
-        session.get(url_logout)
+        session.get(urlLogout)
+
+    return betTask
+
+def find(betTask):
+    city = "Анапа"
+    cityRe = r"%s\b" % city
+    for i in range(len(betTask)):
+        if re.findall(cityRe, str(betTask[i].get('cityOut'))) == [city]:
+            urlBet = str(betTask[i].get('urlBet10'))
+            session.post()
+            break
+
+    session.get(urlLogout)
 
 def main():
     auth()
-    find()
+    parser()
+    find(betTask)
 
 if __name__ == '__main__':
     main()
