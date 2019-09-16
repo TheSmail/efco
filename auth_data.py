12d84091c0a13a6bb9b4b70a8e4d6c4b52cb3234
thesmail <smail.ytn@gmail.com>
2019-09-16 21:33:22 +0300

    up

---
+++ auth_data.py
@@ -0,0 +1,13 @@
+import keyring
+def get_pass():
+    login = "evgen28rus@mail.ru"
+    passwd = keyring.get_password("system", login)
+    data = {'xLogin':login, 'xPassword':passwd}
+    return data
+
+def main():
+    #get_pass()
+    keyring.set_password("system", "evgen28rus@mail.ru", "had2911")
+
+if __name__ == '__main__':
+    main()
